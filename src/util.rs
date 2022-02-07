4b0fbc8e5dff11ccc0d7cc36c3cb52016c4237a2
arthurfiorette <arthur.fiorette@gmail.com>
2022-02-07 16:50:41 -0300

    refactor: clippy

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,6 @@
 use std::{env, path::Path};
 
-pub fn fallback_rust_log(def: &String) {
+pub fn fallback_rust_log(def: &str) {
   let log_env = env::var("RUST_LOG");
   if log_env.is_err() || log_env.unwrap().is_empty() {
     env::set_var("RUST_LOG", def);
@@ -15,7 +15,7 @@ pub fn fallback_rust_log(def: &String) {
 }
 
 /// Returns the absolute path and the filename
-pub fn normalize_path(raw: &String) -> (String, String) {
+pub fn normalize_path(raw: &str) -> (String, String) {
   let path = Path::new(raw);
 
   // There has to be a better way
