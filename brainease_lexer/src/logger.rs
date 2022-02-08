b7536a3cb408e4868c01f1ecc9c292ca47c7a10f
arthurfiorette <arthur.fiorette@gmail.com>
2022-02-08 17:01:38 -0300

    refactor: better error messages

--- brainease_lexer/src/logger.rs
+++ brainease_lexer/src/logger.rs
@@ -1,14 +1,14 @@
 pub fn unknown_line(line_index: &usize, line: &str) {
-  log::error!("Couldn't recognize line {} \"{}\"", line_index + 1, line);
+  log::error!("Couldn't recognize line {}. \"{}\"", line_index + 1, line);
 }
 
 pub fn extra_characters(line_index: &usize, extra: &str) {
-  log::warn!("Extra characters at line {}. \"{}\"", line_index + 1, extra);
+  log::warn!("Extra characters at line {}, \"{}\".", line_index + 1, extra);
 }
 
 pub fn value_too_big(line_index: &usize, val: &usize, max: usize) {
   log::error!(
-    "Got value too big at line {}. {}, maximum is {}",
+    "Got value too big at line {}. {}, maximum is {}.",
     line_index + 1,
     val,
     max
@@ -17,8 +17,16 @@ pub fn value_too_big(line_index: &usize, val: &usize, max: usize) {
 
 pub fn invalid_char(line_index: &usize, char: &char) {
   log::error!(
-    "Invalid character at line {} character {}",
+    "Invalid character at line {} character {}.",
     line_index + 1,
     char
+  );
+}
+
+pub fn unknown_indentation(line_index: &usize, indentation: &usize) {
+  log::error!(
+    "Unmatched indentation at line {}. Expected more than 2, got {}.",
+    line_index + 1,
+    indentation,
   );
 }
